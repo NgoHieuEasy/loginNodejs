0c398c929b1da9f2ebcbf55f8d448a704e4fc027
Mac <mac@MacBook.local>
2024-10-02 10:31:15 +0700

    listening

---
+++ data.js
@@ -0,0 +1,17 @@
+const data = [
+  {
+    sdt: "+84967358396",
+    apiId: 11872065,
+    apiHash: "8861fb52ec03f3e43e73611f10d926cd",
+  },
+  {
+    sdt: "+84862289117",
+    apiId: 20437980,
+    apiHash: "cc68e8f362f82867f997cc670c048880",
+  },
+  {
+    sdt: "+84382507792",
+    apiId: 23415111,
+    apiHash: "12f7ac0fc718d7afbef4deb99c42e39f",
+  },
+];
--- index.js
+++ index.js
@@ -3,42 +3,74 @@ const { StringSession } = require("telegram/sessions");
 const { NewMessage } = require("telegram/events");
 const readline = require("readline");
 const input = require("input");
+const fs = require("fs");
+const apiId = 23415111;
+const apiHash = "12f7ac0fc718d7afbef4deb99c42e39f";
+const phoneNumber = "+84382507792";
 
-const apiId = 20437980;
-const apiHash = "cc68e8f362f82867f997cc670c048880";
-const stringSession = new StringSession("");
-const phoneNumber = "+84862289117";
+const sessionFile = "session.txt";
 
-// const client = new TelegramClient(stringSession, Number(apiId), apiHash, {
-//   connectionRetries: 5,
-// });
+const savedSession = async () => {
+  const stringSession = new StringSession("");
 
-(async () => {
   const client = new TelegramClient(stringSession, apiId, apiHash, {
     connectionRetries: 5,
   });
 
   try {
+    await client.connect();
     // Kết nối đến Telegram
+    await client.start({
+      phoneNumber: async () => phoneNumber,
+      password: async () => await input.text("Mật khẩu (nếu có)?"),
+      phoneCode: async () => await input.text("Nhập mã OTP?"),
+      onError: (err) => console.log(err),
+    });
+
+    console.log("Đăng nhập thành công!");
+
+    // Lưu session vào file session.txt
+    const sessionString = client.session.save();
+    fs.writeFileSync(sessionFile, sessionString, "utf-8");
+    console.log(`Session đã được lưu vào ${sessionFile}`);
+  } catch (error) {
+    console.error("Đăng nhập thất bại:", error.message);
+  }
+
+  await client.disconnect();
+};
+
+const generateOTP = async () => {
+  console.log("Starting generateOTP function...");
+
+  const token = fs.readFileSync(sessionFile, "utf-8");
+  const stringSession = new StringSession(token);
+  const client = new TelegramClient(stringSession, apiId, apiHash, {
+    connectionRetries: 5,
+  });
+
+  try {
     await client.connect();
 
-    // Kiểm tra xem người dùng đã được ủy quyền hay chưa
     if (!(await client.checkAuthorization())) {
-      // Đăng nhập
-      await client.start({
-        phoneNumber: async () => phoneNumber, // Sử dụng số điện thoại cố định
-        password: async () => await input.text("Password (nếu có)?"), // Nhập mật khẩu nếu cần
-        phoneCode: async () => {
-          // Yêu cầu mã OTP
-          const code = await input.text("Nhập mã OTP từ Telegram: ");
-          return code;
-        },
-        onError: (err) => console.log("Lỗi:", err),
-      });
-
-      console.log("Đăng nhập thành công!");
+      console.log("Session không hợp lệ, cần đăng nhập lại.");
     } else {
       console.log("Bạn đã đăng nhập!");
+
+      client.addEventHandler(async (update) => {
+        console.log("Received update:", update);
+        const messageText = update?.message?.message;
+        if (messageText) {
+          console.log("Received message:", messageText);
+          const otpMatch = messageText.match(/\b(\d{5})\b/);
+          if (otpMatch) {
+            const otpCode = otpMatch[0];
+            console.log(`Mã OTP của ${phoneNumber} là: ${otpCode}`);
+          }
+        }
+      }, new NewMessage({ fromUsers: 777000 })); // Không có điều kiện cụ thể
+
+      console.log("Lắng nghe tin nhắn...");
     }
   } catch (error) {
     console.error("Lỗi trong quá trình đăng nhập:", error.message);
@@ -46,4 +78,8 @@ const phoneNumber = "+84862289117";
     // Đóng kết nối nếu cần
     // await client.disconnect();
   }
+};
+
+(async () => {
+  generateOTP();
 })();
